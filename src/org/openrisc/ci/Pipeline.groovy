e138de4e1e362d2c6f1f77ccd524fa6464ea5b51
Nancy Chauhan <nancychn1@gmail.com>
2019-08-18 19:54:41 +0530

    Update Pipeline.groovy

--- src/org/openrisc/ci/Pipeline.groovy
+++ src/org/openrisc/ci/Pipeline.groovy
@@ -14,23 +14,5 @@ class Pipeline {
         sh 'docker images'
     }
 
-// This method runs or1k-tests against various enviroment variables
-    def dockerRun(TESTING-NAME, JOB, SIM, PIPELINE, EXPECTED_FAILURES, EXTRA_CORE_ARGS ){
-        stage("$TESTINGNAME") {
-            Environment {
-                JOB = "$JOB"
-                SIM = "$SIM"
-                PIPELINE = "$PIPELINE"
-                EXPECTED_FAILURES = "$EXPECTED_FAILURES"
-                EXTRA_CORE_ARGS =  "$EXTRA_CORE_ARGS"
-            }
-            steps {
-                dockerrun()
-            }
-        }
-    }
-
-    void dockerrun(){
-        sh 'docker run --rm -v $(pwd):/src -e "JOB=$JOB" -e "SIM=$SIM" -e "PIPELINE=$PIPELINE" -e "EXPECTED_FAILURES=$EXPECTED_FAILURES" -e "EXTRA_CORE_ARGS=$EXTRA_CORE_ARGS" librecores/librecores-ci-openrisc /src/.travis/test.sh'    
-    }
+// This method runs or1k-tests against various enviroment variable
 }
