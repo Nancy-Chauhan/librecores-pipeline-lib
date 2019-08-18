8bc04252401b9999f5403618ba5538284d0c331c
Nancy Chauhan <nancy.ext@Admins-MacBook-Pro-8.local>
2019-08-18 18:08:46 +0530

    update to tap

---
+++ src/org/openrisc/ci/Pipeline.groovy
@@ -0,0 +1,36 @@
+package org.openrisc.ci
+
+class Pipelne {
+
+    def steps
+
+    Pipelne(steps) {
+        this.steps = steps
+    }
+
+// This method pulls standard dockerimage for openrisc related projects . 
+    def dockerPull(){
+        sh 'docker pull librecores/librecores-ci-openrisc'
+        sh 'docker images'
+    }
+
+// This method runs or1k-tests against various enviroment variables
+    def dockerRun(TESTING-NAME, JOB, SIM, PIPELINE, EXPECTED_FAILURES, EXTRA_CORE_ARGS ){
+        stage("$TESTINGNAME") {
+            Environment {
+                JOB = "$JOB"
+                SIM = "$SIM"
+                PIPELINE = "$PIPELINE"
+                EXPECTED_FAILURES = "$EXPECTED_FAILURES"
+                EXTRA_CORE_ARGS =  "$EXTRA_CORE_ARGS"
+            }
+            steps {
+                dockerrun()
+            }
+        }
+    }
+
+    void dockerrun(){
+        sh 'docker run --rm -v $(pwd):/src -e "JOB=$JOB" -e "SIM=$SIM" -e "PIPELINE=$PIPELINE" -e "EXPECTED_FAILURES=$EXPECTED_FAILURES" -e "EXTRA_CORE_ARGS=$EXTRA_CORE_ARGS" librecores/librecores-ci-openrisc /src/.travis/test.sh'    
+    }
+}
