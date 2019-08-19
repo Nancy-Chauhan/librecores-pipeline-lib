505a86f315a097f56e7b1b7ce007c7fc379c61aa
Nancy Chauhan <nancychn1@gmail.com>
2019-08-19 20:44:10 +0530

    Update Pipeline.groovy

--- src/org/openrisc/ci/Pipeline.groovy
+++ src/org/openrisc/ci/Pipeline.groovy
@@ -1,5 +1,9 @@
 package org.openrisc.ci
 
+def shellExecute(command) {
+    sh command
+}
+
 class Pipeline {
 
     def steps
@@ -10,8 +14,8 @@ class Pipeline {
 
 // This method pulls standard dockerimage for openrisc related projects . 
     def dockerPull(){
-        sh 'docker pull librecores/librecores-ci-openrisc'
-        sh 'docker images'
+        shellExecute('docker pull librecores/librecores-ci-openrisc')
+        shellExecute('docker images')
     }
 
 // This method runs or1k-tests against various enviroment variable
