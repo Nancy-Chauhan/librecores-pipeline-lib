1a7c8fd15a1b8eef5d96f660eaf5f2b7f2a85886
Nancy-Chauhan <nancychn1@gmail.com>
2019-08-20 00:10:22 +0530

    jenkinsfile

--- src/org/openrisc/ci/Pipeline.groovy
+++ src/org/openrisc/ci/Pipeline.groovy
@@ -9,23 +9,30 @@ class Pipeline {
     }
 
     // This method pulls standard dockerimage for openrisc related projects .
-    def dockerPull(){
+    def dockerPull() {
         steps.sh 'docker pull librecores/librecores-ci-openrisc'
         steps.sh 'docker images'
     }
 
-    def dockerRun( TEST , JOB , SIM,PIPELINE , EXPECTED_FAILURES ,  EXTRA_CORE_ARG ) {
-      stage(${Test}) {
-                    environment {
-                        JOB = ${JOB}
-                        SIM = ${SIM}
-                        PIPELINE = ${PIPELINE}
-                        EXPECTED_FAILURES= ${EXPECTED_FAILURES}
-                        EXTRA_CORE_ARGS = ${ EXTRA_CORE_ARG}
-                    }
-                    steps {
-                      steps.sh 'docker run --rm -v $(pwd):/src -e "JOB=$JOB" -e "SIM=$SIM" -e "PIPELINE=$PIPELINE" -e "EXPECTED_FAILURES=$EXPECTED_FAILURES" -e "EXTRA_CORE_ARGS=$EXTRA_CORE_ARGS" librecores/librecores-ci-openrisc /src/.travis/test.sh'
-                    }
+    def dockerRun(test, job, sim = '', pipeline = '', expectedFailures = '', extraCoreArgs = '') {
+        stage(test) {
+//            environment {
+//                JOB = job
+//                SIM = sim
+//                PIPELINE = pipeline
+//                EXPECTED_FAILURES = expectedFailures
+//                EXTRA_CORE_ARGS = extraCoreArgs
+//            }
+            envVars = "-e \"JOB=${job}\" " +
+                    "-e \"SIM=${sim}\" " +
+                    "-e \"PIPELINE=${pipeline}\"" +
+                    " -e \"EXPECTED_FAILURES=${expectedFailures}\" " +
+                    "-e \"EXTRA_CORE_ARGS=${extraCoreArgs}\""
+            image = 'librecores/librecores-ci-openrisc'
+            def command = "/src/.travis/test.sh"
+            steps {
+                steps.sh "docker run --rm -v \$(pwd):/src ${envVars} ${image} ${command}"
+            }
         }
     }
 }
